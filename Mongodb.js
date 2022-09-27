22758b50172a95a84d24c86b54009b112ba32c58
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-09-27 22:05:38 +0530

    Add files via upload

---
+++ Mongodb.js
@@ -0,0 +1,13 @@
+const {MongoClient} =  require("mongodb");
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url)
+
+async function getData()
+{
+    let result = await client.connect();
+    let db = result.db('e-comm')
+    let collection = db.collection('products');
+    let response = await collection.find({name: 'mukesh'}).toArray();
+    console.log(response);
+}
+getData();
