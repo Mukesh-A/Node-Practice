9ca6c2e3df8d5e8db55fbaede57ca42176843f7a
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-09-30 22:28:48 +0530

    Add files via upload

--- API/api.js
+++ API/api.js
@@ -1,11 +1,42 @@
-const express = require('express');
-const dbConnect = require('../mongodb');
+const express = require("express");
+const dbConnect = require("../mongodb");
+const mongodb = require("mongodb");
 const app = express();
 
-app.get('/', async(req,res)=>{
-    let data = await dbConnect()
-    data = await data.find().toArray()
-    console.log(data)
-    res.send(data)
-})
-app.listen(2345)
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.log(data);
+  res.send(data);
+});
+
+app.post("/", async (res, req) => {
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  console.log(result);
+});
+
+app.put("/", async (req, resp) => {
+  let data = await dbConnect();
+  let result = await data.updateOne(
+    {
+      name: req.body.name,
+    },
+    { $set: req.body }
+  );
+  console.log(result);
+  resp.send({ result: "update" });
+});
+
+app.delete("/:id", async (req, res) => {
+  console.log(req.params.id);
+  let data = await dbConnect();
+  const result = await data.deleteOne({
+    _id: new mongodb.ObjectId(req.params.id),
+  });
+  res.send(result);
+});
+
+app.listen(5000);
