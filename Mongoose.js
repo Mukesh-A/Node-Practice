b5c4a6057fc058dc140f4a7bd9afa69a05558054
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-10-01 22:31:29 +0530

    Add files via upload

--- Mongoose.js
+++ Mongoose.js
@@ -1,15 +1,38 @@
 const mongoose = require("mongoose");
-const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017/e-comm");
-  const ProductSchema = new mongoose.Schema({
-    name:String
-  });
+mongoose.connect("mongodb://localhost:27017/e-comm");
 
-  const ProductsModel = mongoose.model('products',ProductSchema);
-  let data = new ProductsModel({name:"m8"});
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+});
+
+const main = async () => {
+  const ProductsModel = mongoose.model("products", ProductSchema);
+  let data = new ProductsModel({ name: "m8" });
   let result = await data.save();
 
-console.log(result)
+  console.log(result);
 };
 
-main()
+const UpdateInDb = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = await Product.updateOne(
+    { name: "iphone" },
+    { $set: { price: 10 } }
+  );
+  console.log(data);
+};
+
+const deleteInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = await Product.deleteOne({ name: "iphone" });
+  console.log(data);
+};
+
+const findInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  const data = await Product.find({ name: "Kumar" });
+  console.log(data);
+};
+
+findInDB();
