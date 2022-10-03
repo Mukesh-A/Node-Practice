cdc34390ed12d60555dcd7c9b3a71199f6c887f2
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-10-03 21:40:08 +0530

    Add files via upload

--- Multer/index.js
+++ Multer/index.js
@@ -3,17 +3,20 @@ const multer = require("multer");
 const app = express();
 
 const upload = multer({
-    storage:multer.diskStorage({
-        destination:function(req,file,cb){
-            cb(null,"uploads")
-        },
-        filename:function(req,file,cb){
-            cb(null,file.fieldname+"-"+Date.now()+".jpg")
-        }
-    })
-})
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+      console.log("1");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+      console.log("2");
+    }
+  })
+}).single("user_file");
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload, (req, res) => {
   res.send("file uploaded");
 });
 app.listen(5000);
+ 
